233068cabb674dad2dc78d6e1cc15c7215414f4f
신현우 <healim5028@naver.com>
2025-07-29 14:04:01 +0900

    feat: cicd 컨트롤러 내용 삭제

--- apps/backend/src/cicd/cicd.controller.ts
+++ apps/backend/src/cicd/cicd.controller.ts
@@ -7,41 +7,6 @@ import { ApiResponse } from 'src/common/dto/response.dto';
 @Controller('cicd')
 export class CicdController {
   constructor(private readonly service: CicdService) { }
-
-  @Post()
-  create(
-    @Body() dto: CreateCicdConfigDto,
-  ): Promise<ApiResponse> {
-    return this.service.create(dto);
-  }
-
-  @Get()
-  findByUser(
-    @Query('userCode') userCode: string,
-  ): Promise<ApiResponse> {
-    return this.service.findByUser(userCode);
-  }
-
-  @Get(':id')
-  findOne(
-    @Param('id') id: string,
-  ): Promise<ApiResponse> {
-    return this.service.findOne(id);
-  }
-
-  @Put(':id')
-  update(
-    @Param('id') id: string,
-    @Body() dto: UpdateCicdConfigDto,
-  ): Promise<ApiResponse> {
-    return this.service.update(id, dto);
-  }
-
-  @Delete(':id')
-  remove(
-    @Param('id') id: string,
-  ): Promise<void> {
-    return this.service.remove(id);
-  }
+  
 
 }
